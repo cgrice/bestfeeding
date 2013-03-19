6349b189fdf7cb3a4f9f3c31f802ee1446d91d8f
Chris Grice <chris@chrisgrice.com>
2013-03-19 17:29:26 +0000

    Starting to sketch out models

--- seymour/feeds/models.py
+++ seymour/feeds/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Feed(models.Model):
+
+    feed_url = models.URLField(max_length=512)
+    etag = models.TextField(null=True) 
+    last_modified = models.DateTimeField(null=True)
