c9ddb16e8b13c8b803abe42c6ed839cb9eb222ec
Chris Grice <chris@chrisgrice.com>
2013-05-28 01:36:15 +0100

    Fixing dj_database_url for heroku

--- bestfeeding/settings/base.py
+++ bestfeeding/settings/base.py
@@ -28,7 +28,7 @@ ADMINS = (
 MANAGERS = ADMINS
 
 DATABASES = {
-    'default': dj_database_url.parse(get_env_variable('DJANGO_DATABASE_URL'))
+    'default': dj_database_url.config()
 }
 
 # Local time zone for this installation. Choices can be found here:
--- bestfeeding/settings/local.py
+++ bestfeeding/settings/local.py
@@ -5,6 +5,10 @@ from .base import *
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
+DATABASES = {
+    'default': dj_database_url.parse(get_env_variable('DJANGO_DATABASE_URL'))
+}
+
 HAYSTACK_CONNECTIONS = {
     'default': {
         'ENGINE': 'haystack.backends.elasticsearch_backend.ElasticsearchSearchEngine',
