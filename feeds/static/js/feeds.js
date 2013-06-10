12b5059762fcbbc44a930d9a7a1c7f47b3fa71cc
Chris Grice <chris@chrisgrice.com>
2013-06-10 17:09:17 -0400

    Removing stats link and fade out on deletes

--- feeds/static/js/feeds.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-    $('.feeds .delete').on('click', function(event) {
-        event.preventDefault();
-        $(this).closest('tr').fadeOut();
-        if($(this).closest('.feeds').find('tr').length <= 0) {
-            $(this).closest('.feeds').remove();
-        }
-    });
-});
