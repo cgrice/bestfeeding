e5eb535b48678f8c8f60288dcf4f2cf1d2c01b81
Chris Grice <chris@chrisgrice.com>
2013-06-10 12:23:12 -0400

    Removing table if all feeds removed for day

--- feeds/static/js/feeds.js
+++ feeds/static/js/feeds.js
@@ -1,5 +1,9 @@
 $(document).ready(function() {
     $('.feeds .delete').on('click', function(event) {
+        event.preventDefault();
         $(this).closest('tr').fadeOut();
+        if($(this).closest('.feeds').find('tr').length <= 0) {
+            $(this).closest('.feeds').remove();
+        }
     });
 });
